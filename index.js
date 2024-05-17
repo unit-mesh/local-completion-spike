a8d3e7b5b5ff6c2b0460884540938ff731117f9a
Phodal Huang <h@phodal.com>
2024-05-17 15:38:40 +0800

    chore: update template

--- index.js
+++ index.js
@@ -1,15 +1,22 @@
 const path = require('path');
 
 (async () => {
-  const { env, AutoTokenizer } = await import('@xenova/transformers')
+  const { env, AutoTokenizer, LlamaTokenizer } = await import('@xenova/transformers')
+  const ortPromise = import('onnxruntime-node')
   env.allowLocalModels = true
 
-  let modelPath = path.join('..', '..', '..', 'models', 'flcc.model')
-  let modelBase = path.join('..', '..', '..', 'models', '')
+  let modelPath = path.join('models', 'flcc.model')
+  let modelBase = path.join('..', '..', '..', '..', 'models', '')
 
-  let tokenizer = await AutoTokenizer.from_pretrained(modelBase, {
+  let tokenizer = await LlamaTokenizer.from_pretrained(modelBase, {
     quantized: true,
     local_files_only: true,
   })
+
+  const ort = await ortPromise
+  ort.default.InferenceSession.create(modelPath, { executionProviders: ['cpu'] }).then(async (ortSession) => {
+
+  })
+
 })()
 
